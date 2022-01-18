58c741837f797b6252d97a7d91e4f3d6819ce6a4
Shayan Talebian <shayantalebianwork@gmail.com>
2022-01-18 10:14:55 +0330

    Create Card component

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,6 +1,31 @@
 import React from "react";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import styles from "./Cards.module.css";
+const Cards = (props) => {
+  console.log(props);
 
-const Cards = () => {
-  return <h1>Chart</h1>;
+  return (
+    <div className={styles.container}>
+      <Grid
+        container
+        spacing={3}
+        justify="center
+      "
+      >
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Infected
+            </Typography>
+            <Typography variant="h5">REAL DATA</Typography>
+            <Typography color="textSecondary">READL DATA</Typography>
+            <Typography variant="body2">
+              Number of active cases of COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 export default Cards;
